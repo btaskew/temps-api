c4028203b69650cb5e48b4ece84a90ded951b5e3
baskew <b.askew@exeter.ac.uk>
2018-04-16 15:23:14 +0100

    Add initial jobs tests

---
+++ tests/JobsTest.php
@@ -0,0 +1,31 @@
+<?php
+
+class JobsTest extends TestCase
+{
+    /** @test */
+    public function can_query_for_all_jobs()
+    {
+        $jobs = factory('App\Job', 3)->create();
+
+        $this->get('/jobs')
+            ->seeJsonContains([
+                'id' => $jobs[0]->id,
+                'title' => $jobs[1]->title,
+                'description' => $jobs[2]->description
+            ]);
+    }
+
+    /** @test */
+    public function can_query_for_a_single_job()
+    {
+        $job = factory('App\Job')->create();
+        $otherJob = factory('App\Job')->create();
+
+        $this->get("/jobs/$job->id")
+            ->seeJsonContains([
+                'title' => $job->title
+            ])->dontSeeJson([
+                'title' => $otherJob->title
+            ]);
+    }
+}
