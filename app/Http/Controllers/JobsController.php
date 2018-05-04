b6f1bae603c4400c916b9de31cc65011216ae6d4
baskew <b.askew@exeter.ac.uk>
2018-05-04 13:01:38 +0100

    Add pagination to jobs query

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -19,7 +19,7 @@ class JobsController extends Controller
     {
         $jobs = Job::latest()->open()->withVacancies()->filter($filters);
 
-        return $this->respond($jobs->get());
+        return $this->respond($jobs->paginate(10));
     }
 
     /**
